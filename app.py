e0f43ff096a91cb7e26d860774ea8cc819ff17b1
Gareth Mellon <garethmellon@gmail.com>
2019-04-04 17:40:13 +0100

    Set app form to save details to MongoDB and provide user feedback.

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from mongo_connect import mongo_connect
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        data = ({"fname": request.form['fname'], "sname": request.form['sname'], "email": request.form['email']})
+        try:
+            coll = mongo_connect()
+            coll.insert_one(data)
+            flash("Your data has been submitted")
+        except Exception as ex:
+            flash("There was an error with the data {}".format(ex))
+
+        return redirect(url_for('home'))
+
     return render_template("index.html")
 
+
 if __name__ == '__main__':
+    app.secret_key = os.environ.get('secret_key')
     app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
